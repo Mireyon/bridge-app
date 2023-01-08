c457d06e4e5f5bd84cbb36edd228a1c0e282c96e
Mirey <remy.xia@edu.esiee.fr>
2023-01-08 16:47:10 +0100

    Update

--- bridge-api/app.py
+++ bridge-api/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request
 from flask_cors import CORS
-from process_data import add_db, get_db
+from process_data import add_db, get_db, delete_db
 
 app = Flask(__name__)
 CORS(app)
@@ -19,5 +19,11 @@ def GetData():
     id = request.args.get('id')
     return get_db(id)
 
+@app.route('/data', methods=['DELETE'])
+def DeleteData():
+    id = request.args.get('id')
+    return delete_db(id)
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
+    # app.run(debug=True)
--- bridge-api/process_data.py
+++ bridge-api/process_data.py
@@ -1,17 +1,24 @@
+import base64
+from PIL import Image
+import io
+
 class Database:
     data_first = []
     data_second = []
+    data_manager = [data_first, data_second]
     
-    def add_data(data):
-        if(data.id==1):
-            Database.data_first.append(python2json(data))
-        else:
-            Database.data_second.append(python2json(data))
+    def add_data(id, data):
+        Database.data_manager[id].append(python2json(data))
 
     def get_data(id):
-        if(id==1):
-            return {"data": Database.data_second}
-        return {"data":Database.data_first}
+        return {"data": Database.data_manager[abs(id-1)]}
+
+    def delete_data(id):
+        data = Database.data_manager[abs(id-1)]
+        if(len(data)!=0):
+            data.pop(0)
+            return "Last message deleted", 204
+        return "There was no message", 204
 
 class DataGroup:
     def __init__(self, data) -> None:
@@ -28,10 +35,36 @@ def python2json(data):
             "text": data.text,
             "image": data.image,}
 
+def base64ToBMP(image_base64):
+    # image_base64 = image_base64.split("base64,")[1]
+    image_data = base64.b64decode(image_base64)
+    img = Image.open(io.BytesIO(image_data))
+    img = img.resize((64, 64))
+    #Threshold
+    # img = img.point( lambda p: 255 if p > 128 else 0 )
+    img.show()
+
+    output = io.BytesIO()
+    img.save(output, 'BMP')
+
+    # Get the resized image data
+    image_data = output.getvalue()
+
+    image_hex = image_data.hex()
+    image_bmp = []
+    for i in range(0, len(image_hex), 2):
+        image_bmp.append(f'0x{image_hex[i:i+2]}')
+
+    return image_bmp
+
 def add_db(payload):
     data = json2python(payload)
-    Database.add_data(data)
+    data.image = base64ToBMP(data.image)
+    Database.add_data(id=data.id, data=data)
     return 'Message added', 200
 
 def get_db(id):
-    return Database.get_data(id)
+    return Database.get_data(id=int(id))
+
+def delete_db(id):
+    return Database.delete_data(id=int(id))
